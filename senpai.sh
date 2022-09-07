39ec5e2300f4f4dfa1e4df110e1eb1d1ead9b675
Samuel Lord <sam@sllord.info>
2022-09-07 15:27:49 -0500

    Update senpai.sh

--- senpai.sh
+++ senpai.sh
@@ -1,3 +1,2 @@
 echo Please wait... (This will take awhile....)
-convert $1 -filter Jinc -resize 1000% -auto-level $1-10x.png
-
+MAGICK_OCL_DEVICE=true convert $1 -filter Jinc -resize 1000% -auto-level $1-10x.png
