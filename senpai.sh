1b607461ac078743d103445bc52805f0483e4a6a
Samuel Lord <sam@sllord.info>
2022-09-07 15:29:20 -0500

    Update senpai.sh

--- senpai.sh
+++ senpai.sh
@@ -1,2 +1,2 @@
-echo Please wait... (This will take awhile....)
+echo "Please wait... (This will take awhile....)"
 MAGICK_OCL_DEVICE=true convert $1 -filter Jinc -resize 1000% -auto-level $1-10x.png
